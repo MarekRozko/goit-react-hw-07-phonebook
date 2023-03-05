ff73986e351ed2a0736147b6f4edce20f6582b71
Marek Rożko <maknamar12@gmail.com>
2023-03-05 15:39:41 +0100

    add fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,20 @@
 import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit"; 
-import { persistStore } from 'redux-persist';
+import { persistStore,FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER } from 'redux-persist';
 
 import rootReducer from "./root-reducer";
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware({
-    serializableCheck: false,
-  }),
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
 })
 
 export const persistor = persistStore(store);
